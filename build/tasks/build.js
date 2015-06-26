81a177960e8cced9f4723de6a3128b631ff626a3
Az <azim.palmer@googlemail.com>
2015-06-26 17:23:26 +0100

    adding new build file

--- build/tasks/build.js
+++ build/tasks/build.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var changed = require('gulp-changed');
 var runSequence = require('run-sequence');
 var to5 = require('gulp-babel');
 var paths = require('../paths');
@@ -8,6 +9,13 @@ var assign = Object.assign || require('object.assign');
 gulp.task('build-es6', function () {
   return gulp.src(paths.source)
     .pipe(gulp.dest(paths.output + 'es6'));
+});
+
+// copies changed html files to the output directory
+gulp.task('build-html', function () {
+  return gulp.src(paths.html)
+    .pipe(changed(paths.output, {extension: '.html'}))
+    .pipe(gulp.dest(paths.output + 'system'));
 });
 
 gulp.task('build-commonjs', function () {
@@ -31,7 +39,7 @@ gulp.task('build-system', function () {
 gulp.task('build', function(callback) {
   return runSequence(
     'clean',
-    [/* 'build-es6', 'build-commonjs', 'build-amd',*/ 'build-system'],
+    [/* 'build-es6', 'build-commonjs', 'build-amd',*/ 'build-system','build-html'],
     callback
   );
 });
